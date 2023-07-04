a7709f9d102fa6a3b9336bb203388b73217dd1f3
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-04 16:48:49 +0100

    initial commit

---
+++ account-setup.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("account-setup-form");
+const firstName = document.getElementById("firstname");
+console.dir(firstName);
+const lastName = document.getElementById("lastname");
+const username = document.getElementById("username");
+const avatarURL = document.getElementById("avatarurl");
+const fields = [firstName, lastName, username, avatarURL];
+
+const errorMessage = document.getElementsByClassName("error");
+const firstNameError = document.getElementById("firstname-error");
+const lastNameError = document.getElementById("lastname-error");
+
+form.addEventListener("submit", toggleSubmit);
+firstName.addEventListener("blur", checkText);
+lastName.addEventListener("blur", checkText);
+form.addEventListener("submit", toggleSubmit);
+
+function toggleSubmit(event) {
+	if (!fields.every((field) => field.classList.includes("valid")))
+		event.preventDefault();
+}
+
+function checkText(event) {
+	const inputs = event.target;
+	if (/^[a-zA-Z]+$/.test(inputs.value)) {
+		inputs.classList.add("valid");
+		inputs.classList.remove("invalid");
+		[...errorMessage].map((message) => {
+			message.innerText = "";
+		});
+	} else {
+		inputs.classList.add("invalid");
+		inputs.classList.remove("valid");
+		if (inputs.id === "firstname")
+			firstNameError.innerText = "Please enter your first name";
+		if (inputs.id === "lastname")
+			lastNameError.innerText = "Please enter your last name";
+	}
+}
